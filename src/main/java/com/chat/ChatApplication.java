f341fbcab7eb520cc6189ab9b39a171615a51648
aadawi <ahmad@otg.ae>
2018-04-10 08:38:07 +0400

    commit

--- src/main/java/com/chat/ChatApplication.java
+++ src/main/java/com/chat/ChatApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatApplication {
-
 	public static void main(String[] args) {
 		System.out.println("START APP");
 		SpringApplication.run(ChatApplication.class, args);
